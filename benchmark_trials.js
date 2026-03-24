70660893c9e682da8113d493fd2e2c4fde68bd38
Arthur Yong <50041948+arthurjyong@users.noreply.github.com>
2026-03-24 21:48:06 +0800

    Preserve SEARCH_PROGRESS score precision in I:R formatting

--- benchmark_trials.js
+++ benchmark_trials.js
@@ -261,7 +261,7 @@ function applyBenchmarkReviewNumberFormatting_(sheet) {
     return;
   }
 
-  sheet.getRange("I:R").setNumberFormat("0");
+  sheet.getRange("I:R").setNumberFormat("0.############");
   sheet.getRange("F:F").setNumberFormat("0.00");
   sheet.getRange("G:G").setNumberFormat("0.00");
   sheet.getRange("H:H").setNumberFormat("0.00");
